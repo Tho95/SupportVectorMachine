2162814ebd86fd13ba9b21bccb02fd50d9d29271
Thomas Huebscher <hubscherthomas@yahoo.de>
2021-02-11 14:42:19 +0100

    created first SVM model

--- main.py
+++ main.py
@@ -3,6 +3,8 @@
 
 from sklearn.datasets import load_iris
 import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.svm import SVC
 
 import dataInfo
 import plot
@@ -28,10 +30,16 @@ X.drop('target', axis = 1, inplace=True)
 print(X.head())
 print("target-values:", iris.target_names)
 
+X_train, X_valid, y_train, y_valid = train_test_split(X, y, train_size=0.6, test_size=0.4,random_state=9)
+
+#####model
+model = SVC(kernel='rbf',)
+
+model.fit(X_train,y_train)
+print("Score: ", model.score(X_valid, y_valid))
+predictions = model.predict(X_valid)
 
 
-
-###################### we try to find extra data
 
 
 
